c179fbce6cbb8420b20b06facddda2964e570fa3
Leo M Loncarovich <leol.mosca@gmail.com>
2023-03-30 18:32:50 -0300

    Organizando os exercicios

---
+++ parafixar1.js
@@ -0,0 +1,27 @@
+// Desenvolva em cada exercicio a seguir com o uso de funções.
+
+// Adicione um valor ao saldo
+let saldo = 3500;
+function adicionaUmValor (valor){
+  return saldo + valor
+}
+
+//Subtraia um valor do saldo.
+function subtraiUmValor (valor){
+  return saldo - valor
+}
+
+//Multiplique um valor do saldo.
+function multipliqueUmValor (valor){
+  return saldo * valor 
+}
+
+//Divida um valor do saldo.
+function dividaUmValor (valor){
+  return saldo / valor
+}
+
+console.log(adicionaUmValor(500));
+console.log(subtraiUmValor(500));
+console.log(multipliqueUmValor(2));
+console.log(dividaUmValor(2));
