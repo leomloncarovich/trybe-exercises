f01bb4e4256acdc963b987b61e82bbc2e21a3c08
Leo M Loncarovich <leol.mosca@gmail.com>
2023-05-23 18:58:56 -0300

    Copy display

--- GeradorDeSenhas/src/main.js
+++ GeradorDeSenhas/src/main.js
@@ -1,4 +1,6 @@
 import { nanoid } from 'nanoid';
+import './style.css';
+import copy from 'clipboard-copy';
 
 const btn = document.querySelector('button');
 const display = document.querySelector('h2');
@@ -6,4 +8,9 @@ const display = document.querySelector('h2');
 btn.addEventListener('click', () => {
   const randomPassword = nanoid();
   display.innerHTML = randomPassword;
+});
+
+display.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
 });
