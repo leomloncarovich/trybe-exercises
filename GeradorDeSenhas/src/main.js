462a6db975699318b072c57b4e53152ec13060d9
Leo M Loncarovich <leol.mosca@gmail.com>
2023-05-23 17:55:54 -0300

    Gerador de senhas

---
+++ GeradorDeSenhas/src/main.js
@@ -0,0 +1,9 @@
+import { nanoid } from "nanoid";
+
+const btn = document.querySelector('button');
+const display = document.querySelector('h2');
+
+btn.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  display.innerHTML = randomPassword;
+});
