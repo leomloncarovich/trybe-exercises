6c6fb8eba30aeb36932a82d27e197d0815a1ef60
Leo M Loncarovich <leol.mosca@gmail.com>
2024-02-06 21:16:09 -0300

    feat: Material do primeiro dia de node.js

---
+++ modulos-node/app/index.js
@@ -0,0 +1,7 @@
+const { celsiusToFahrenheit, fahrenheitToCelsius } = require('../mymodules/temp');
+
+const temp = require('../mymodules/temp');
+console.log(temp.celsiusToFahrenheit(37));
+console.log(temp.fahrenheitToCelsius(37));
+
+console.log(celsiusToFahrenheit(36))
